a628e795b45480a92b535cfe5d24c24a27a81eaf
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2024-03-06 14:37:01 +0530

    M

--- global.js
+++ global.js
@@ -249,7 +249,7 @@ currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png" width="1024" height="768" alt=""/></div><div class="wrap1"><div class="wrap2"><div class="s1_2"><img src="slide1/s1_2.png"/></div></div><div class="s1_3"><img src="slide1/s1_3.png"/></div></div><div class="s1_4"><img src="slide1/s1_4.gif"/></div><div class="s1_5"><img src="slide1/s1_5.png" width="1024" height="768" alt=""/></div><div class="s1_6"><img src="slide1/s1_6.png"/></div><div class="fuseIn" onclick="fuseIn()">';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png" width="1024" height="768" alt=""/></div><div class="wrap1"><div class="wrap2"><div class="s1_2"><img src="slide1/s1_2.png"/></div></div><div class="s1_3"><img src="slide1/s1_3.png"/></div></div><div class="s1_4"><img src="slide1/s1_4.png"/></div><div class="s1_5"><img src="slide1/s1_5.png" width="1024" height="768" alt=""/></div><div class="s1_6"><img src="slide1/s1_6.png"/></div><div class="fuseIn" onclick="fuseIn()">';
 	break;
 
 }
